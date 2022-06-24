2040f79f4ffb90edd3d4e0044cb1ad809d94b8e7
eurus-eastwind <86453147+eurus-eastwind@users.noreply.github.com>
2022-06-24 01:45:52 -1100

    added home routes with contact us and feeatured dentist section

--- backend/src/routes/home.routes.js
+++ backend/src/routes/home.routes.js
@@ -3,9 +3,20 @@
  */
 const router = require("express").Router();
 
-// // import controller 
-// const loginController = require('../controllers/login.controller');
-// router.post('/login', loginController.login);
+// import index controller 
+// GET all the featured doctor from DB with images section
+// contact us using nodemailer controller, bottom section
+const homeController = require('../controllers/home/index.controller');
+router.get('/', homeController.render);
+
+// Featured Dentist section but still in the home page part
+const dentistsController = require('../controllers/home/index.controller');
+router.get('/', dentistsController.getAllDentist);
+
+// For contact us, Test this one after connection of frontend
+const contactUsController = require('../controllers/home/contactUs.controller');
+router.post('/send', contactUsController.send);
+
 
 
 module.exports = router;
