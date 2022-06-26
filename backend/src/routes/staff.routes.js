33cd6352bc18f092c13f06554c384b556f2c7866
Jhay Mark Pineda <106953483+pjhaymark@users.noreply.github.com>
2022-06-27 02:11:37 +0800

    staff routes
    updates staff's routes
    
    di ko alam kung tama ba to HAHAHAHAHA

---
+++ backend/src/routes/staff.routes.js
@@ -0,0 +1,10 @@
+var router = require('express').Router();
+
+//import invoices controller
+//const invoicesController = require("../controllers/invoices.controller");
+
+//router.post("/", invoicesController.create); //insert
+//router.put("/", invoicesController.update); //update
+//router.get("/invoices_id", invoicesController.findOne); //find specific id
+
+//module.exports = router;
