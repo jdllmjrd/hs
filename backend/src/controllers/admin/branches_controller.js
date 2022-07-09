2496786e26e119696a0e605e834b0bef72541a5a
eurus-eastwind <86453147+eurus-eastwind@users.noreply.github.com>
2022-07-08 14:18:27 -1100

    branches

--- backend/src/controllers/admin/branches_controller.js
+++ backend/src/controllers/admin/branches_controller.js
@@ -1,38 +1,33 @@
 const db = require("../../models");
 const Branches = db.Branches;
-const Users = db.Users;
+const Users = db.Users
 const { dataResponse, emptyDataResponse, checkAuthorization, errResponse } = require('../../helpers/helper.controller');
 
 
-//create and save new branch -- checked
+//create and save new branch
 exports.createBranches = (req, res) => {
 
    req.body.branches_created_by = req.user.users_id;
-   req.body.branches_image= req.file != undefined ? req.file.filename : "";
    // Check users-type if valid
    checkAuthorization(req, res, "Admin");
 
-    // Create Branch
-    Branches.create(req.body,{ 
-            include: ["added_branch"] 
-        })
-        .then((data) => {
-            Users.findByPk(data.users_id, { 
-                include: 
-                    ["added_branch"],
-                }).then(
-                    (result) => {
-                    res.send({
-                        error: false,
-                        data: result,
-                        message: [process.env.SUCCESS_CREATE],
-                     });
-                }
-            );
-            }).catch((err) => errResponse(res, err)); 
-        }
-
-// Update Branches -- checked
+   Branches
+       .findOne({ where: { branches_name: req.body.branches_name}})
+       .then(result => {
+           if(result) emptyDataResponse(res, "")
+           else {
+               // Set id
+               req.body.branches_id = req.branches_id;
+               
+               // Create Branch
+               Branches
+                   .create(req.body,{ include: ["created"] })
+                   .then((data) => dataResponse(res, data, "A new record has been added", "Record is not added"))
+                   .catch((err) => errResponse(res, err)); 
+           }
+       }) .catch(err => helper.errResponse(res, err));
+};
+// Update Branches
 exports.updateBranches = (req, res) => {
 
     // Check if user-status is valid
@@ -44,22 +39,24 @@ exports.updateBranches = (req, res) => {
             where: {
                branches_id: req.params.branches_id
             },
+            include: ["updated"]
         })
         .then(data => dataResponse(res, data, "Updated Successfully", "No updates happened"))
         .catch(err => errResponse(res, err))
 }
-// Get all Branches -- checked
+// Get all Branches
 exports.getAllBranches = (req, res, next) => {
     
     // Check authorization first
     checkAuthorization(req, res, "Admin")
 
     Branches
-        .findAll()
+        .findAll({ where: { branches_status: "Open"}})
         .then(data => dataResponse(res, data, "Branches Retrieved Successfully", "No Branch has been retrieved"))
         .catch(err => errResponse(res, err));
 }
-// Deactivate Branch -- checked
+// Deactivate Branch in order to be not seen on frontend
+
 exports.deleteBranch = (req, res) => {
     const body = { branches_status: "Close" };
     const branches_id = req.params.branches_id;
@@ -73,4 +70,3 @@ exports.deleteBranch = (req, res) => {
         })
         .catch(err => errResponse(res, err));
 }
-
