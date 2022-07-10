abd6a04683e593e56ca68be28a888b786741b3b5
eurus-eastwind <86453147+eurus-eastwind@users.noreply.github.com>
2022-07-10 04:52:59 -1100

    added controller for services

--- backend/src/routes/service.routes.js
+++ backend/src/routes/service.routes.js
@@ -5,4 +5,6 @@ const servicesController = require('../controllers/service.controller');
 router.get('/', servicesController.getServices);
 router.get('/:services_id', servicesController.getServices);
 
+router.get('/', servicesController.send);
+
 module.exports = router;
