ea3f2ca3cdd012f8b7c6d273c118e983dbc5ed23
eurus-eastwind <86453147+eurus-eastwind@users.noreply.github.com>
2022-07-14 08:02:08 +0800

    Revert "Revert "update directory and code""
    This reverts commit 4c3235ebe31c5235c35386d4bff0853fec993a39.

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 // import modules/packages
 const express = require("express");
-const dotenv = require("dotenv").config();
-const db = require("./backend/src/models");
-const jwt = require("jsonwebtoken");
-const path = require("path");
-var cors = require("cors");
+const db = require("./src/models");
+const cors = require("cors");
 
-//initialize app
-var app = express();
-
-// parse requests of content-type - application/json
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: true }));
+require("dotenv").config();
 
 // For token secret
 // console.log(require("crypto").randomBytes(64).toString("hex"));
@@ -45,97 +36,19 @@ if (process.env.ALLOW_SYNC === "true") {
     });
 }
 
-//   Check PORT
+//initialize app
+var app = express();
+
+// parse requests of content-type - application/json
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// all request will go here first (middleware)
+app.use(cors());
+app.use(require("./src/middleware/tokenValidator"));
+app.use(require("./src/routes"));
+
 const PORT = process.env.PORT || 5600;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
-// all request will go here first (middleware)
-app.use(cors());
-app.use((req, res, next) => {
-  // console.log("Request has been sent to " + req.url);
-  next();
-});
-
-// Authentication for TOKEN
-// next - is for next function to run
-const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers["authorization"]; // Bearer
-  const token = authHeader && authHeader.split(" ")[1];
-
-  // If token is null then send unauthorized response
-  if (token == null)
-    return res.status(401).send("No access token is detected.");
-
-  // Verify the token, if not verified then forbidden
-  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
-    // If token is not verified then send forbidden response
-    if (err) {
-      if (process.env.ENABLE_ACCESS_TOKEN_LOG === "true")
-        console.log(`${err}\n`);
-      return res.sendStatus(403);
-    }
-
-    req.user = user;
-    console.log(user);
-    next();
-  });
-};
-
-/*** ROUTES*/
-// Images
-app.use(
-  "/users-profile-pic",
-  express.static(
-    path.join(__dirname + "./backend/src/public/users_profile_pic")
-  )
-);
-app.use(
-  "/featured_dentist",
-  express.static(path.join(__dirname + "./backend/src/public/featured_dentist"))
-);
-app.use(
-  "/services",
-  express.static(path.join(__dirname + "./backend/src/public/services"))
-);
-// Home page
-app.use(
-  `${process.env.API_VERSION}/home`,
-  require("./backend/src/routes/home.routes")
-);
-// Register Page
-app.use(
-  `${process.env.API_VERSION}/register`,
-  require("./backend/src/routes/register.routes")
-);
-// For log in page
-app.use(
-  `${process.env.API_VERSION}/login`,
-  require("./backend/src/routes/login.routes")
-);
-//Service page
-app.use(
-  `${process.env.API_VERSION}/services`,
-  require("./backend/src/routes/service.routes")
-);
-
-/** FOR FOUR USER TYPES ROUTEs */
-// Authenticated Routes
-//app.use(`${process.env.API_VERSION}/patient`, authenticateToken , require("./backend/src/routes/patient.routes"));
-app.use(
-  `${process.env.API_VERSION}/staff`,
-  authenticateToken,
-  require("./backend/src/routes/staff.routes")
-);
-app.use(
-  `${process.env.API_VERSION}/dentist`,
-  authenticateToken,
-  require("./backend/src/routes/dentist.routes")
-);
-
-/** Admin user route*/
-app.use(
-  `${process.env.API_VERSION}/admin`,
-  authenticateToken,
-  require("./backend/src/routes/admin.routes")
-);
---
+++ src/middleware/tokenValidator.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+
+const excludeUrl = [
+  "/users-profile-pic",
+  "/featured_dentist",
+  "/services",
+  `/${process.env.API_VERSION}/register`,
+  `/${process.env.API_VERSION}/login`,
+  `/${process.env.API_VERSION}/services`,
+];
+
+const isRequiredToken = (path) => {
+  return excludeUrl.filter((url) => path.startsWith(url)).length == 0;
+};
+
+module.exports = (req, res, next) => {
+  if (isRequiredToken(req.path)) {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+    if (token == null) {
+      return res.status(401).send("No access token is detected.");
+    }
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+      if (err) {
+        if (process.env.ENABLE_ACCESS_TOKEN_LOG === "true") {
+          console.log(`${err}\n`);
+        }
+        return res.sendStatus(403);
+      }
+      req.user = user;
+    });
+  }
+  next();
+};
