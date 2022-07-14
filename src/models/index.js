c448803adc23bd01c507b0f9fdb798dd0ecdd410
eurus-eastwind <86453147+eurus-eastwind@users.noreply.github.com>
2022-07-14 02:42:44 -1100

    config

--- src/models/index.js
+++ src/models/index.js
@@ -5,7 +5,8 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'production';
-const config = require(__dirname + '../config/config.json')[env]; 
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
@@ -29,9 +30,7 @@ Object.keys(db).forEach(modelName => {
     db[modelName].associate(db);
   }
 });
-// ASSOCIATIONS 
 
-//this is for users_branches
 
 
 
