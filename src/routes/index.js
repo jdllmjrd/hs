f7696732c5c50a6f36ca59bc2fce8aadfe53aeef
eurus-eastwind <86453147+eurus-eastwind@users.noreply.github.com>
2022-07-14 10:02:29 +0800

    remove duplicate

--- src/routes/index.js
+++ src/routes/index.js
@@ -35,6 +35,4 @@ router.use(`/${process.env.API_VERSION}/admin`, require("./admin.routes"));
 
 router.use(`/${process.env.API_VERSION}/staff`, require("./staff.routes"));
 
-app.use(`${process.env.API_VERSION}/staff`, require("./staff.routes"));
-
 module.exports = router;
