7092f695a16dbc5385152dbc22aad60bc1bd6050
Judell L. Mejorada <mejoradajudell15@gmail.com>
2022-08-04 20:52:01 +0800

    added recoverpw and changepw route

--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -3,5 +3,7 @@ const router = require("express").Router();
 // import controller user
 const loginController = require('../controllers/login.controller');
 router.post('/', loginController.login);
+router.post('/recoverpw', loginController.recoverpw);
+router.post('/changepw', loginController.changepw);
 
 module.exports = router;
