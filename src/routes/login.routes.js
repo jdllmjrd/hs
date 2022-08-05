20da24837fe35cb076cf956abc1b124d97fa28ed
eurus-eastwind <86453147+eurus-eastwind@users.noreply.github.com>
2022-08-05 12:34:35 +0800

    fix error

--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 
 // import controller user
-const loginController = require('../controllers/login.controller');
-router.post('/', loginController.login);
-router.post('/recoverpw', loginController.recoverpw);
-router.post('/changepw', loginController.changepw);
+const loginController = require("../controllers/login.controller");
+router.post("/", loginController.login);
+router.post("/recoverpw", loginController.recoverpw);
+router.post("/changepw", loginController.newpw);
 
 module.exports = router;
