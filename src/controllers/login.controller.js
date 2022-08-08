c27bfe2f55eb913f8e2d3272c28eb9ca1ea7e16a
eurus-eastwind <86453147+eurus-eastwind@users.noreply.github.com>
2022-08-08 23:54:52 +0800

    update login

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -6,7 +6,7 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const db = require("../models");
-const nodemailer = require("nodemailer");;
+const nodemailer = require("nodemailer");
 const Users = db.Users;
 const {
   errResponse,
@@ -19,10 +19,7 @@ const generateToken = (data) => {
 };
 
 exports.login = (req, res) => {
-  if (
-    String(req.body.users_email) === "" ||
-    String(req.body.users_password) === ""
-  ) {
+  if (String(req.body.users_email) === "") {
     res.status(500).send({
       error: true,
       data: [],
@@ -36,33 +33,62 @@ exports.login = (req, res) => {
     .then((data) => {
       if (data) {
         // compare password
-        bcrypt.compare(
-          req.body.users_password,
-          data.users_password,
-          function (err, result) {
-            if (result) {
-              // same password
-              res.send({
-                error: false,
-                data: data,
-                token: generateToken({
-                  users_id: data.users_id,
-                  users_full_name: data.users_full_name,
-                  users_email: data.users_email,
-                  users_type: data.users_type,
-                }),
-                message: [process.env.SUCCESS_RETRIEVED],
-              });
-            } else {
-              // invalid password
-              res.status(500).send({
-                error: true,
-                data: [],
-                message: ["Invalid username and Password."],
-              });
-            }
+        if (req.body.users_google_id) {
+          if (req.body.users_google_id === data.users_google_id) {
+            res.send({
+              error: false,
+              data: data,
+              token: generateToken({
+                users_id: data.users_id,
+                users_full_name: data.users_full_name,
+                users_email: data.users_email,
+                users_type: data.users_type,
+              }),
+              message: [process.env.SUCCESS_RETRIEVED],
+            });
+          } else {
+            res.status(500).send({
+              error: true,
+              data: [],
+              message: ["Invalid username and Password."],
+            });
           }
-        );
+        } else {
+          if (!req.body.users_password) {
+            res.status(500).send({
+              error: true,
+              data: [],
+              message: ["Username or Password is empty."],
+            });
+          }
+          bcrypt.compare(
+            req.body.users_password,
+            data.users_password,
+            function (err, result) {
+              if (result) {
+                // same password
+                res.send({
+                  error: false,
+                  data: data,
+                  token: generateToken({
+                    users_id: data.users_id,
+                    users_full_name: data.users_full_name,
+                    users_email: data.users_email,
+                    users_type: data.users_type,
+                  }),
+                  message: [process.env.SUCCESS_RETRIEVED],
+                });
+              } else {
+                // invalid password
+                res.status(500).send({
+                  error: true,
+                  data: [],
+                  message: ["Invalid username and Password."],
+                });
+              }
+            }
+          );
+        }
       } else {
         res.status(500).send({
           error: true,
@@ -83,12 +109,12 @@ exports.login = (req, res) => {
 };
 
 exports.recoverpw = (req, res) => {
-  const {email} = req.body;
+  const { email } = req.body;
 
   if (String(email) === "" && email === null) {
     res.status(400).body({
       error: true,
-      message: process.env.GENERAL_ERROR_MSG
+      message: process.env.GENERAL_ERROR_MSG,
     });
     return;
   }
@@ -97,51 +123,53 @@ exports.recoverpw = (req, res) => {
     service: "gmail",
     auth: {
       user: process.env.AUTH_EMAIL,
-      pass: process.env.AUTH_PASSWORD 
-    }
+      pass: process.env.AUTH_PASSWORD,
+    },
   });
 
-const link = "https://happysmile-web.herokuapp.com" || process.env.BASE_URL;
+  const link = "https://happysmile-web.herokuapp.com" || process.env.BASE_URL;
 
-const options = {
-  from: process.env.AUTH_EMAIL,
-  to: email,
-  subject: 'Recover Password',
-  text: `<p>Click the link to redirect to recover password <a href="${link}/Change/pages-changepw.php?email=${email}">Link</a></p>`
-};
+  const options = {
+    from: process.env.AUTH_EMAIL,
+    to: email,
+    subject: "Recover Password",
+    text: `<p>Click the link to redirect to recover password <a href="${link}/Change/pages-changepw.php?email=${email}">Link</a></p>`,
+  };
 
-transporter.sendMail(options, (err, data) => {
-  if (err) {
-    res.status(500).body({
-      message: process.env.GENERAL_ERROR_MSG
-    });
-  } else {
-    console.log(data.response);
-  }
-});
-
+  transporter.sendMail(options, (err, data) => {
+    if (err) {
+      res.status(500).body({
+        message: process.env.GENERAL_ERROR_MSG,
+      });
+    } else {
+      console.log(data.response);
+    }
+  });
 };
 
 exports.newpw = (req, res) => {
-  const {password, email} = req.body;
+  const { password, email } = req.body;
 
   if (String(password) === "" && password === null) {
     res.status(400).body({
       error: true,
-      message: process.env.GENERAL_ERROR_MSG
+      message: process.env.GENERAL_ERROR_MSG,
     });
     return;
   }
 
   const hash = bcrypt.hashSync(password, parseInt(process.env.SALT_ROUND));
 
-  Users.update({users_password: hash}, {
-    where: {
-      users_email: email
+  Users.update(
+    { users_password: hash },
+    {
+      where: {
+        users_email: email,
+      },
     }
-  });
+  );
 
   res.status(200).body({
-    message: process.env.SUCCESS_CREATE
+    message: process.env.SUCCESS_CREATE,
   });
 };
